ac74640bea21b3d0491fb7d1c4bb6aeb7495b4e9
AshV <ashish6318@gmail.com>
2021-04-01 23:34:16 +0530

    added beautify feature

--- js/script.js
+++ js/script.js
@@ -161,3 +161,33 @@ document.querySelector('#save').addEventListener('click', () => {
     }
     downloadToFile(editor.getCode(), fileName, 'text/plain');
 });
+
+var prettifyXml = function (sourceXml) {
+    var xmlDoc = new DOMParser().parseFromString(sourceXml, 'application/xml');
+    var xsltDoc = new DOMParser().parseFromString([
+        // describes how we want to modify the XML - indent everything
+        '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
+        '  <xsl:strip-space elements="*"/>',
+        '  <xsl:template match="para[content-style][not(text())]">', // change to just text() to strip space in text nodes
+        '    <xsl:value-of select="normalize-space(.)"/>',
+        '  </xsl:template>',
+        '  <xsl:template match="node()|@*">',
+        '    <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
+        '  </xsl:template>',
+        '  <xsl:output indent="yes"/>',
+        '</xsl:stylesheet>',
+    ].join('\n'), 'application/xml');
+
+    var xsltProcessor = new XSLTProcessor();
+    xsltProcessor.importStylesheet(xsltDoc);
+    var resultDoc = xsltProcessor.transformToDocument(xmlDoc);
+    var resultXml = new XMLSerializer().serializeToString(resultDoc);
+    return resultXml;
+};
+
+document.querySelector('#beautify').addEventListener('click', () => {
+    debugger;
+    var ugly = editor.getCode();
+    var prettyXml = prettifyXml(ugly);
+    editor.setCode(prettyXml);
+});
